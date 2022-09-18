05c19a8b5b15e95402a97f4ac4a398884f0b0919
emortal <me@emortal.dev>
2022-09-18 21:43:56 +0100

    Revert "better chest loot"
    This reverts commit 693aa1954bd97bde1b5c9a22208d99e47e8d0dca.

--- src/main/kotlin/dev/emortal/doors/game/ChestLoot.kt
+++ src/main/kotlin/dev/emortal/doors/game/ChestLoot.kt
@@ -1,14 +1,12 @@
 package dev.emortal.doors.game
 
 import dev.emortal.doors.block.ChestHandler
-import dev.emortal.doors.util.placeItem
 import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.Component
 import net.minestom.server.coordinate.Point
 import net.minestom.server.entity.Player
 import net.minestom.server.instance.block.Block
 import net.minestom.server.inventory.Inventory
-import net.minestom.server.inventory.InventoryType
 import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
 import net.minestom.server.sound.SoundEvent
@@ -17,17 +15,17 @@ import world.cepi.kstom.util.setItemStack
 object ChestLoot {
     private val lootList = listOf<(Inventory) -> Boolean>(
         // Coins
-        { inv -> probability(60) { inv.placeItem(ItemStack.of(Material.SUNFLOWER).withAmount((1..7*2).random().floorDiv(3))) } },
-        { inv -> probability(27) { inv.placeItem(ItemStack.of(Material.GOLD_INGOT).withAmount((1..4*2).random().floorDiv(3))) } },
-        { inv -> probability(9) { inv.placeItem(ItemStack.of(Material.DIAMOND)) } },
+        { inv -> probability(40) { inv.setItemStack(4, 1, ItemStack.of(Material.SUNFLOWER)) } },
+        { inv -> probability(20) { inv.setItemStack(2, 0, ItemStack.of(Material.GOLD_INGOT)) } },
+        { inv -> probability(10) { inv.setItemStack(1, 0, ItemStack.of(Material.DIAMOND)) } },
         // Tools
-        { inv -> probability(19) { inv.placeItem(ItemStack.of(Material.FLINT_AND_STEEL).withMeta { it.damage((1..60).random()) }) } },
+        { inv -> probability(13) { inv.setItemStack(6, 1, ItemStack.of(Material.FLINT_AND_STEEL)) } },
         // Timothy
-        { inv -> probability(2) { inv.placeItem(ItemStack.of(Material.SPIDER_SPAWN_EGG)) } }
+        { inv -> probability(2) { inv.setItemStack(5, 0, ItemStack.of(Material.SPIDER_SPAWN_EGG)) } }
     )
 
     private fun generateLoot(inventory: Inventory) {
-        lootList.shuffled().forEach { loot ->
+        lootList.forEach { loot ->
             if(loot(inventory)) {
                 return
             }
@@ -48,9 +46,6 @@ object ChestLoot {
 
         if (!handler.generatedLoot.get()) {
             generateLoot(handler.inventory)
-            if (handler.inventory.inventoryType == InventoryType.CHEST_4_ROW) {
-                generateLoot(handler.inventory)
-            }
             handler.generatedLoot.set(true)
         }
 
--- src/main/kotlin/dev/emortal/doors/util/InventoryHelpers.kt
+++
@@ -1,6 +0,0 @@
-package dev.emortal.doors.util
-
-import net.minestom.server.inventory.Inventory
-import net.minestom.server.item.ItemStack
-
-fun Inventory.placeItem(item: ItemStack) = setItemStack((0 until size).random(), item)
